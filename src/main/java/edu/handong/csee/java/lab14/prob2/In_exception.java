1765eeea4edf7ef5ac36f7ac598bdcb8c693220f
Isaac Chae <qpalzmm22@gmail.com>
2018-05-08 15:26:25 +0900

    Source code for p2 complete

---
+++ src/main/java/edu/handong/csee/java/lab14/prob2/In_exception.java
@@ -0,0 +1,35 @@
+package edu.handong.csee.java.lab14.prob2;
+
+import java.util.*;
+
+public class In_exception {
+	private int x;
+	private int y;
+	
+	public In_exception() {
+	}
+	
+	public void twoNumberDivider() {
+		try {
+			Scanner keyboard = new Scanner(System.in);
+			System.out.print("x: ");
+			this.x = keyboard.nextInt();
+			System.out.print("y: ");
+			this.y = keyboard.nextInt();
+			System.out.println(this.x/this.y);
+		}
+		catch(ArithmeticException e) {
+			System.out.println("java.lang.ArithmeticException: " + e.getMessage());
+		}
+		catch(InputMismatchException e) {
+			System.out.println("java.util.InputMismatchExeption");
+		}
+		catch(Exception e) {
+			System.out.println("Other error"+ e.getMessage());
+		}
+	}
+	public static void main(String args[]) {
+		In_exception exceptionDetector= new In_exception();
+		exceptionDetector.twoNumberDivider(); 
+	}
+}
