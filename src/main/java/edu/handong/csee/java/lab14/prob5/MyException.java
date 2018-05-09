31c60e7bb68b1e52e00c6ee2f663d19738828661
Isaac Chae <qpalzmm22@gmail.com>
2018-05-09 22:08:30 +0900

    Single line comments for MyException

--- src/main/java/edu/handong/csee/java/lab14/prob5/MyException.java
+++ src/main/java/edu/handong/csee/java/lab14/prob5/MyException.java
@@ -1,19 +1,19 @@
-package edu.handong.csee.java.lab14.prob5;
+package edu.handong.csee.java.lab14.prob5; // makes it a package
 
-public class MyException extends Exception{
-	private String message;
-	public MyException() {
-		super();
+public class MyException extends Exception{	// this class inherits Exception
+	private String message;	// this variable stores message for exception
+	public MyException() {	// default constructor
+		super();	// use default constructor of Exception
 	}
-	public MyException(String message) {
-		super(message);
-		this.message = message;
+	public MyException(String message) {	// a constructor with String
+		super(message);	// uses constructor with String of Exception
+		this.message = message;	// sets the message by given String
 	}
 	
-	public MyException(Throwable cause) {
-		super(cause);
+	public MyException(Throwable cause) {	// a constructor with Throwable
+		super(cause);	// uses constructor with Throwable of Exception
 	}
-	public String toString() {
-		return message;
+	public String toString() {	// this method gives the exception messsage 
+		return message;	// returns exception message
 	}
 }
