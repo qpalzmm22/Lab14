9cebeebb8bbd10520c6f13cedba13a062235a8ab
Isaac Chae <qpalzmm22@gmail.com>
2018-05-09 23:38:26 +0900

    Single line comments added

--- src/main/java/edu/handong/csee/java/lab14/prob6/CustomID.java
+++ src/main/java/edu/handong/csee/java/lab14/prob6/CustomID.java
@@ -1,25 +1,25 @@
-package edu.handong.csee.java.lab14.prob6;
+package edu.handong.csee.java.lab14.prob6;	// makes it a package
 
-public class CustomID extends Exception{
-	private String[] races = {"Vulcans", "Romulan", "Klingons"};
+public class CustomID extends Exception{	// this class inherits Exception class
+	private String[] races = {"Vulcans", "Romulan", "Klingons"};	// kinds of races
 	
-	public void setName(String name) throws Exception{
-		if(name.length() < 5)
-			throw new Exception("Your name is short! Try again!");
+	public void checkName(String name) throws Exception{	// this method throws exception when found
+		if(name.length() < 5)	// when length of the name is less than 5
+			throw new Exception("Your name is short! Try again!");	// throw following exception
 	}
 	
-	public void setAge(int age) throws Exception{
-		if(age < 18)
-			throw new Exception("You are too young! Try again!");
+	public void checkAge(int age) throws Exception{	// this method throws exception when found
+		if(age < 18)	// when age is less than 18
+			throw new Exception("You are too young! Try again!");	//throw following exception
 	}
 	
-	public void setRace(String race) throws Exception{
-		boolean validRace = false;
-		for(String r : races) {
-			if(r.equalsIgnoreCase(race))
-				validRace = true;
+	public void checkRace(String race) throws Exception{	// this method throws exception when found
+		boolean validRace = false;	// a variable to check if the race is valid
+		for(String r : races) {	// for all the races in the races
+			if(r.equalsIgnoreCase(race))	// if the races contain race
+				validRace = true;	// it's valid race
 		}
-		if(!validRace)
-			throw new Exception(race + "! Try again.");
+		if(!validRace)	// if it's not valid race
+			throw new Exception(race + "! Try again.");	// throw exception
 	}
 }
--- src/main/java/edu/handong/csee/java/lab14/prob6/Main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob6/Main.java
@@ -1,59 +1,59 @@
-package edu.handong.csee.java.lab14.prob6;
+package edu.handong.csee.java.lab14.prob6;	// makes it a package
 
-import java.util.*;
+import java.util.*;	// import java
 
-public class Main {
-
-	public static void main(String[] args) {
-		boolean validName = false,
-				validAge = false, 
-				validRace = false;
+public class Main {	// this class tests the CustomID
+	public static void main(String[] args) {	// main method to check if according exceptions are thrown
+		boolean validName = false,	// a variable to check if it's valid name
+				validAge = false, 	// a variable to check if it's valid age
+				validRace = false;	// a variable to check if it's valid race
 		
-		CustomID racePlayer = new CustomID();
-		Scanner keyboard = new Scanner(System.in);
+		CustomID racePlayer = new CustomID();	// instantiate CusomID to use setName(), setAge(), setRace()
+		Scanner keyboard = new Scanner(System.in);	// instantiate Scanner to use nextInt(), nextLine() 
 		
-		while((!validName || !validAge) || !validRace) {
-			if(!validName){
-				try {
-					System.out.print("Enter your name: ");
-					String name = keyboard.nextLine();
-					racePlayer.setName(name);
-					System.out.println("Name is valid");
-					System.out.println("Name: " + name);
-					validName = true;
+		while((!validName || !validAge) || !validRace) {	// while name, age, or race is not valid
+			if(!validName){	// if name is invalid
+				try {	// try to find exception
+					System.out.print("Enter your name: "); // ask user for name
+					String name = keyboard.nextLine();	// receives name and stores it
+					racePlayer.checkName(name);	//	check if the name passes the exception test
+					System.out.println("Name is valid");	// print that the name passed the test
+					System.out.println("Name: " + name);	// print the name 
+					validName = true;						// change the state of validName to true so this sections is not called anymore
 				}
-				catch(Exception e) {
-					System.out.println(e.getMessage());
-					continue;
+				catch(Exception e) {						// if any exception was thrown
+					System.out.println(e.getMessage());		// print the exception message
+					continue;								// start the loop fresh
 				}
 			}
 			if(!validAge){
 				try {
-					System.out.print("Enter your age: ");
-					int age = keyboard.nextInt();
-					racePlayer.setAge(age);
-					System.out.println("Age is valid");
-					System.out.println("Age: "+ age);
-					validAge = true;
+					System.out.print("Enter your age: ");	// ask user for age
+					int age = keyboard.nextInt();	// receives age and stores it
+					racePlayer.checkAge(age);	//	check if the age passes the exception test
+					System.out.println("Age is valid");	// print that the age passed the test
+					System.out.println("Age: "+ age);	// print the age 
+					validAge = true;					// change the state of validAge to true so this sections is not called anymore
 				}
-				catch(Exception e) {
-					System.out.println(e.getMessage());
-					continue;
+				catch(Exception e) {					// if any exception was thrown
+					System.out.println(e.getMessage());	// print the exception message
+					continue;							// start the loop fresh
 				}
 			}
 			if(!validRace){
 				try {
-					System.out.print("Enter your race: "); 
-					String race = keyboard.next();
-					racePlayer.setRace(race);
-					System.out.println("Race is valid");
-					System.out.println("race: " + race);
-					validRace = true;
+					System.out.print("Enter your race: "); 	// ask user for race
+					String race = keyboard.nextLine();	// receives race and stores it
+					racePlayer.checkRace(race);	//	check if the race passes the exception test
+					System.out.println("Race is valid");	// print that the race passed the test
+					System.out.println("race: " + race);	// print the race 
+					validRace = true;						// change the state of validRace to true so this sections is not called anymore
 				}
-				catch(Exception e) {
-					System.out.println(e.getMessage());
+				catch(Exception e) {						// if any exception was thrown
+					System.out.println(e.getMessage());		// print the exception message
 				}
 			}
 		}
+		keyboard.close(); // clsoe keyboard for safety
 	}
 }
