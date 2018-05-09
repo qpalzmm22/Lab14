737dfd360eb985e310fb2f3fc1f9281a6bb1b0e3
Isaac Chae <qpalzmm22@gmail.com>
2018-05-09 22:08:07 +0900

    single line comment for MyException

---
+++ src/main/java/edu/handong/csee/java/lab14/prob5/MyException.java
@@ -0,0 +1,19 @@
+package edu.handong.csee.java.lab14.prob5;
+
+public class MyException extends Exception{
+	private String message;
+	public MyException() {
+		super();
+	}
+	public MyException(String message) {
+		super(message);
+		this.message = message;
+	}
+	
+	public MyException(Throwable cause) {
+		super(cause);
+	}
+	public String toString() {
+		return message;
+	}
+}
